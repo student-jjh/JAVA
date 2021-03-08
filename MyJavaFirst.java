952ba8d53d7cccd6b03eeed28ac91eebdf274202
student-jjh <78084544+student-jjh@users.noreply.github.com>
2021-03-08 23:03:24 +0900

    Add files via upload
    first java

---
+++ MyJavaFirst.java
@@ -0,0 +1,20 @@
+package myfirst;
+import java.util.Scanner;
+public class MyJavaFirst {
+
+	public static void main(String[] args) {
+	System.out.println("Hello Java!!");
+	System.out.println("Hello Java!!");
+	System.out.println("Hello Java!!");
+	int a,b;
+	int isum;
+	
+	Scanner scan = new Scanner(System.in);
+	a=scan.nextInt();
+	b=scan.nextInt();
+	isum=a+b;
+	System.out.println(isum);
+			
+	}
+
+}
